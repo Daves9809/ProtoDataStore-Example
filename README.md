438be31963aeee68638a7b6f45cb7bfe9c8d1238
Dawid Seroka <dawid.seroka@untitledkingdom.com>
2022-11-14 11:14:41 +0100

    README added

---
+++ README.md
@@ -0,0 +1,14 @@
+# ProtoDataStore-Example
+-------------
+
+## Screenshots & Gifs:
+
+### App performance
+<img src="./gifs/volunteer_panel.gif" width="30%" height="30%">&ensp;<img src="./gifs/protoExample_behave.gif" width="30%" height="30%">
+
+## Installation
+To run this app paste in Constant object your api key from: https://gorest.co.in
+
+## App Architecture
+* MVI
+* Kotlin
--- app/src/main/java/com/example/protoexample/Constant.kt
+++ app/src/main/java/com/example/protoexample/Constant.kt
@@ -1,23 +1,6 @@
 package com.example.protoexample
 
 object Constant {
-    val protoUsers: List<UserProto> = listOf(
-        UserProto
-            .newBuilder()
-            .setId("1")
-            .setEmail("wacek@op.pl")
-            .setGender("Male")
-            .setName("wacek")
-            .setStatus("married")
-            .build(),
-
-        UserProto
-            .newBuilder()
-            .setId("2")
-            .setEmail("Andrzej@op.pl")
-            .setGender("Male")
-            .setName("Andrzej")
-            .setStatus("not-married")
-            .build(),
-    )
+    val BASE_URL = "https://gorest.co.in"
+    val API_KEY = ""
 }
